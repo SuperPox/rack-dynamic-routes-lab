791e33e1c388d20302e7efaf38bda2f6afdadf96
SuperPox <calebkohl@gmail.com>
2021-02-18 20:35:50 -0700

    commit tree 0435783f5b61708c458c202a9eec4cec859f5497

--- app/application.rb
+++ app/application.rb
@@ -7,21 +7,18 @@ class Application
         resp = Rack::Response.new
         req = Rack::Request.new(env)
 
-        #if req.path=="/items/<ITEM NAME>"
-        #if req.path.match(/items/) 
         if req.path.match(/items/)
-            @@items.each do |item|
-                resp.write "#{item}\n"
-
+            item_name = req.path.split("/items/").last
+            if item =@@items.find{|i| i.name == item_name}
+                resp.write item.price
+            else
+                resp.status = 400
+                resp.write "Item not found"
             end
-            binding.pry
-
-            #resp.write "#{ITEM PRICE}"
         else
             resp.write "Route not found"
             resp.status = 404
         end
-
         resp.finish
     end
 end
